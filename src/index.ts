4a3adfef2d4118584d1d68871812781f68996aff
MohammadFakih02 <mohammadfakih025@gmail.com>
2025-01-02 01:37:11 +0200

    updated index to include new endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { Elysia } from 'elysia'
 import { authController } from './controller/auth.controller'
 import { userController } from './controller/user.controller'
+import { computerController } from './controller/computer.controller'
+import { adminController } from './controller/admin.controller'
 import { env } from './config/env'
 import { staticPlugin } from '@elysiajs/static'
 
 const app = new Elysia()
   .use(authController)
   .use(userController)
+  .use(computerController)
+  .use(adminController)
   .use(staticPlugin({
     assets: './uploads',
     prefix: '/uploads'
