282d865609a868f48131ede7c5fdaf35287f8a95
MohammadFakih02 <mohammadfakih025@gmail.com>
2025-01-01 16:29:30 +0200

    added the rest of the get functions

--- src/services/computer.service.ts
+++ src/services/computer.service.ts
@@ -28,5 +28,39 @@ export class ComputerService {
     })
   }
 
+  async getStorage() {
+    return await prisma.storage.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+  }
 
+  async getMotherboards() {
+    return await prisma.motherboard.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+  }
+
+  async getPowerSupplies() {
+    return await prisma.powerSupply.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+  }
+
+  async getCases() {
+    return await prisma.case.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+  }
 }
