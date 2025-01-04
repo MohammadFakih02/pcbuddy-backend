c13ce0bd35cbc436be81bed545f837ecff25eb0a
MohammadFakih02 <mohammadfakih025@gmail.com>
2025-01-04 10:26:30 +0200

    added rate limiter for index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ import { computerController } from './controller/computer.controller'
 import { adminController } from './controller/admin.controller'
 import { env } from './config/env'
 import { staticPlugin } from '@elysiajs/static'
+import { rateLimit } from 'elysia-rate-limit'
 
 const app = new Elysia()
+  .use(rateLimit({
+    duration:60000,
+    max:100,
+  } ))
   .use(authController)
   .use(userController)
   .use(computerController)
