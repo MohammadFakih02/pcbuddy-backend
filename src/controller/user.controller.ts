12de6c320440bcbf77ed5998dbca1432b720aae8
MohammadFakih02 <mohammadfakih025@gmail.com>
2025-01-04 11:03:12 +0200

    added validation to image uploads

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -75,35 +75,33 @@ export const userController = new Elysia()
     }
   )
   .post(
-    "/profile/picture",
+    '/profile/picture',
     async ({ body: { file }, jwt, set, request }) => {
       const payload = await isAuthenticated({ jwt, set, request });
       if (set.status === 401) {
-        return { message: "Unauthorized" };
+        return { message: 'Unauthorized' };
       }
-
+  
+      if (!file.type.startsWith('image/')) {
+        set.status = 400;
+        return { message: 'Only image files are allowed' };
+      }
+  
       try {
         const profilePictureUrl = await saveFile(file);
-
-        const updatedUser = await userService.updateProfilePicture(
-          payload.userId,
-          profilePictureUrl
-        );
-
+        const updatedUser = await userService.updateProfilePicture(payload.userId, profilePictureUrl);
         return updatedUser;
       } catch (error) {
         set.status = 400;
-        return {
-          message:
-            error instanceof Error
-              ? error.message
-              : "Failed to upload profile picture",
-        };
+        return { message: error instanceof Error ? error.message : 'Failed to upload profile picture' };
       }
     },
     {
       body: t.Object({
-        file: t.File(),
+        file: t.File({
+          type: ['image/jpeg', 'image/png', 'image/gif','image/jpg'],
+          maxSize: 8 * 1024 * 1024,
+        }),
       }),
     }
   )
