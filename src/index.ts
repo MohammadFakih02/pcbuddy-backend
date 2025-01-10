cff1de0358f4f5f13719899b507f2675e3bec230
MohammadFakih02 <mohammadfakih025@gmail.com>
2025-01-10 17:09:07 +0200

    added Ai support to the backend

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
-import { Elysia } from 'elysia'
-import { authController } from './controller/auth.controller'
-import { userController } from './controller/user.controller'
-import { computerController } from './controller/computer.controller'
-import { adminController } from './controller/admin.controller'
-import { env } from './config/env'
-import { staticPlugin } from '@elysiajs/static'
-import { rateLimit } from 'elysia-rate-limit'
-import cors from '@elysiajs/cors'
+import { Elysia, t } from 'elysia';
+import { authController } from './controller/auth.controller';
+import { userController } from './controller/user.controller';
+import { computerController } from './controller/computer.controller';
+import { adminController } from './controller/admin.controller';
+import { env } from './config/env';
+import { staticPlugin } from '@elysiajs/static';
+import { rateLimit } from 'elysia-rate-limit';
+import cors from '@elysiajs/cors';
+import { AIController } from './controller/AI.controller';
 
 const app = new Elysia()
   .use(cors())
-  .use(rateLimit({
-    duration:60000,
-    max:1000,
-  } ))
+  .use(
+    rateLimit({
+      duration: 60000,
+      max: 1000,
+    })
+  )
   .use(authController)
   .use(userController)
   .use(computerController)
   .use(adminController)
-  .use(staticPlugin({
-    assets: './uploads',
-    prefix: '/uploads'
-  }))
-  .listen(env.PORT)
+  .use(AIController)
+  .use(
+    staticPlugin({
+      assets: './uploads',
+      prefix: '/uploads',
+    })
+  )
+  .listen(env.PORT);
 
-console.log(`Server is running on http://localhost:${env.PORT}`)
+console.log(`Server is running on http://localhost:${env.PORT}`);
